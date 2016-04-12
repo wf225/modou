c735725fabcfbfd4fd3e9a8cb6309909a1bc6fb3
WuFeng <wf225@126.com>
2016-04-12 21:37:30 +0800

    1.修改定时机制不起作用的bug 2.修改manifest.json文件开始处有两个不可见字符的问题 3.加上启动时的转菊花效果

--- WeatherClient/package/v081_OP/modouweather.sh
+++ WeatherClient/package/v081_OP/modouweather.sh
@@ -1,8 +1,10 @@
 ﻿#!/bin/sh
 # version=0.8.2
 
+. /lib/functions/app.sh 
+
 CURDIR=$(cd $(dirname $0) && pwd)
-APP_WCLOCK="/$CURDIR/bin/wclock"
+APP_WCLOCK="$CURDIR/bin/wclock"
 PIDFILE="$CURDIR/conf/custom.pid"
 
 CITYID=""
@@ -10,7 +12,8 @@ URL_WEATHER="http://modouweather.sinaapp.com/services/weather"
 PATH_WEATHER=$CURDIR/"data/weather_info.json"
 DATAJSON="$CURDIR/conf/data.json"
 SETCONF="$CURDIR/data/customset.conf"
-CRON="/data/conf/cron.d/matrix"
+#CRON="/data/conf/cron.d/matrix"
+CRON="/etc/crontabs/matrix"
 
 interval=1s;
 timeout=10;
@@ -51,24 +54,31 @@ download()
 	# 3. download complete, show the new info
 	wait_download "$PATH_WEATHER"
 	ls -l $PATH_WEATHER
+
+        logger "download weather"
 }
 
 start()
 {	
+	start_loading 正在获取天气信息
 	download;
+	finish_loading
 
 	if [ -f "$PATH_WEATHER" ]; then
 		echo "show new weather"
 		
 		#设置自动更新：每30分钟一次
-		sed -i -e '$a\*/30 \* \* \* \* '$CURDIR'/modouweather.sh download >/dev/null' $CRON
-		/system/sbin/crond.sh restart
+		#sed -i -e '$a\*/2 \* \* \* \* '$CURDIR'/modouweather.sh download >/dev/null' $CRON
+		#/system/sbin/crond.sh restart
+
+        echo "*/5 * * * * $CURDIR/modouweather.sh download 1>/dev/null 2>&1 &" >> $CRON
 		
+
 		$APP_WCLOCK
 		
 		#程序退出后删除
 		sed -i '/modouweather.sh download/d' $CRON
-		/system/sbin/crond.sh restart
+		#/system/sbin/crond.sh restart
 	else
 		echo "exit";
 		exit 0;	
